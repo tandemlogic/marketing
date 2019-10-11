d70f0b47876129628d44f5a8e16a1f36accb11d0
Elliot Bonneville <elliot.bonneville@gmail.com>
2019-10-11 18:01:16 +0200

    Configure Prettier and ESLint to work together

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
-  extends: ["airbnb-base", "plugin:react/recommended"],
-  parser: "babel-eslint"
+  extends: ["airbnb-base", "plugin:react/recommended", "prettier"],
+  parser: "babel-eslint",
+  rules: {
+    "prettier/prettier": "error"
+  },
+  extends: ["plugin:prettier/recommended"]
 };

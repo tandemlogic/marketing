e0a97ab0b04ca8002944c288a2d5c6b95b345800
elliot.bonneville <elliot.bonneville@gmail.com>
2019-10-11 13:14:53 +0200

    Install Gatsby

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/elliot/workspace/tandemlogic/marketing/node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/gatsby-icon.png"},
+    },{
+      plugin: require('/Users/elliot/workspace/tandemlogic/marketing/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/async-requires.js
@@ -0,0 +1,10 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/elliot/workspace/tandemlogic/marketing/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("/Users/elliot/workspace/tandemlogic/marketing/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("/Users/elliot/workspace/tandemlogic/marketing/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-page-2-js": () => import("/Users/elliot/workspace/tandemlogic/marketing/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
+}
+
